886d46f57b7f74900ffd7163a042cb984cc3bedf
Noppadon <earthstill2559@gmail.com>
2023-03-06 03:43:57 +0700

    12

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function Sidebar() {
+  return (
+    <ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+  </li>
+</ul>
+  );
+}
+
+export default Sidebar;
